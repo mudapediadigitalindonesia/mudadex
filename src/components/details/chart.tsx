5372b12e2292baa6382d961c272d7f4a0a1c4de7
IIQball <muhammadiqbl45@gmail.com>
2025-04-23 10:40:37 +0700

    restore default main

--- src/components/details/chart.tsx
+++
@@ -1,7 +0,0 @@
-<div className="flex flex-row h-[calc(100vh - 118px)] mt-[2px] min-[500px] border-t-2 border-[#6c6c6c]">
-        <div className="flex flex-1 gap-[2px] w-full">
-          <div className="flex flex-col gap-1 w-[280px] order-1  border-r-2 border-[#6c6c6c]">
-          </div>
-          <div className="flex border border-white">HAI</div>
-        </div>
-</div>
--- src/lib/database/generateUserId.ts
+++
@@ -1,8 +0,0 @@
-import * as rand from 'randomstring'
-
-const generateUserId = () => {
-  const str = rand.generate({length: 16, capitalization: 'uppercase', readable: true})
-  return `USR${str}`
-}
-
-export default generateUserId
