c771df4203cbcbaf48dbf005b955473eb0c2102e
jenny <jennyalfian45@gmail.com>
2025-05-19 10:33:41 +0700

    updateee

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,25 +12,25 @@ export default function Home() {
         className="logo w-[80px] h-[80px]"
       />
       <nav className="flex space-x-6 text-blue-50 dark:text-zinc-950 font-medium">
-        <a href="#" className="hover:text-white">
+        <a href="#" className="hover:text-blue-900">
           BuyCrypto
         </a>
-        <a href="#" className="hover:text-white">
+        <a href="#" className="hover:text-blue-900">
           Trade
         </a>
-        <a href="#" className="hover:text-white">
+        <a href="#" className="hover:text-blue-900">
           Markets
         </a>
-        <a href="#" className="hover:text-white">
+        <a href="#" className="hover:text-blue-900">
           Earn
         </a>
-        <a href="#" className="hover:text-white">
+        <a href="#" className="hover:text-blue-900">
           About
         </a>
       </nav>
       <div className="auth-buttons flex items-center space-x-4 mr-24">
         <ThemeToggle />
-        <button className="login px-4 py-2 rounded-md font-semibold text-blue-950 dark:text-black bg-transparent dark:bg-[#6c5ce7] hover:bg-white hover:text-[#0a0b2c] border border-blue-950 transition dark:border-black">
+        <button className="login px-4 py-2 rounded-md font-semibold text-blue-50 dark:text-black bg-transparent dark:bg-[#6c5ce7] hover:bg-white hover:text-[#0a0b2c] border border-blue-950 transition dark:border-black">
           Login
         </button>
         <button className="signup px-4 py-2 rounded-md font-semibold text-blue-950 bg-[#6c5ce7] dark:bg-transparent hover:bg-indigo-600 transition dark:border dark:border-blue-950 ">
